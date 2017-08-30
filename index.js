32b160b8befbbab33ca570dc943ae4acba2bf43d
Archit1992 <gajjararchit00004@gmail.com>
2017-08-30 00:52:08 -0700

    make it better

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ var mailer = require('nodemailer');
 
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
-    var data = "Mohit, Archit, Manan, Parth";   
+    var data = "Archit_1, Archit_2, Archit_3, Archit_4";
     
     mailer.SMTP = {
         host: 'email-smtp.us-east-1.amazonaws.com', 
         port:587,
         use_authentication: true, 
-        user: 'AKIAJNXDTUJG2YOEABYQ', 
-        pass: 'Ak9kuwUbeN5kDJeJJN85PKrvom0kqp/DZtfnCu5/C10M'
+        user: 'SMTP_user',
+        pass: 'SMTP_pswd'
     };
 
-
+// writing csv string data into csv file.
 fs.writeFile('./archit.csv', data, function (err, result) {
         if (err) {
             console.log("Error is :", err);
@@ -25,8 +25,8 @@ fs.writeFile('./archit.csv', data, function (err, result) {
             console.log('Archit.csv written to disk successfully!');
             fs.readFile("./archit.csv", function (err, data) {
                 mailer.send_mail({
-                    sender: 'architgajjar1992@gmail.com',
-                    to: 'gajjararchit00004@gmail.com',
+                    sender: 'architgajjar@gmail.com',
+                    to: 'gajjararchit@gmail.com',
                     subject: 'Attachment!',
                     body: 'mail content...',
                     attachments: [{'filename': 'archit.csv', 'content': data}]
